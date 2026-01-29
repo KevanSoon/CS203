5b9bc2b74e1d91e979d249b99e928d8b771875f3
Zion <ziontanyien@gmail.com>
2026-01-29 16:06:33 +0800

    CT4-74 Added Lesson, Chapter, Card, Quiz Tables

--- backend/db.sql
+++ backend/db.sql
@@ -10,3 +10,44 @@ CREATE TABLE IF NOT EXISTS user(
   last_login DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   deactivated_at TIMESTAMP NULL
 );
+
+CREATE TABLE IF NOT EXISTS lesson(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255) UNIQUE NOT NULL,
+  description TEXT,
+  status ENUM('saved','pending', 'approved', 'rejected'),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS chapter(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS card(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    front TEXT,
+    back TEXT,
+    display_order TINYINT,
+    lesson_id INT NOT NULL,
+    chapter_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE KEY unique_lesson_chapter (`lesson_id`, `chapter_id`),
+    FOREIGN KEY (`lesson_id`) REFERENCES lesson(`id`),
+    FOREIGN KEY (`chapter_id`) REFERENCES chapter(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS quiz(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    question TEXT,
+    options TEXT,
+    correct_answer VARCHAR(255),
+    card_id INT NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (`card_id`) REFERENCES card(`id`)
+);
