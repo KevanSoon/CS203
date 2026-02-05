1585f9b9a7542d113948cd2f64c9a73283b6a435
Zion <ziontanyien@gmail.com>
2026-02-05 09:07:13 +0800

    CT4-83 added get lesson applications apis

--- backend/src/main/java/com/backend/cs203/repository/LessonRepository.java
+++ backend/src/main/java/com/backend/cs203/repository/LessonRepository.java
@@ -1,7 +1,8 @@
 package com.backend.cs203.repository;
 
 import java.util.List;
-import com.backend.cs203.dto.lesson.LessonsSummaryDTO;
+
+import com.backend.cs203.dto.lesson.*;
 import com.backend.cs203.entity.Lesson;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,6 +10,14 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface LessonRepository extends JpaRepository<Lesson, Long> {
     @Query(value = "SELECT title, description, created_at FROM lesson WHERE status='approved' AND deleted_at IS NULL ORDER BY created_at DESC", nativeQuery = true)
-    List<LessonsSummaryDTO> findAllLessons();
+    List<LessonSummaryDTO> findAllLessons();
+
+    @Query(value = "SELECT title, description, status, created_at FROM lesson WHERE status IN ('pending','rejected') AND deleted_at IS NULL ORDER BY created_at DESC", nativeQuery = true)
+    List<LessonApplicationDTO> findAllLessonApplications();
+
+    @Query(value = "SELECT title, description, created_at FROM lesson WHERE status='pending' AND deleted_at IS NULL ORDER BY created_at DESC", nativeQuery = true)
+    List<LessonSummaryDTO> findAllPendingLessonApplications();
+
+
 
 }
