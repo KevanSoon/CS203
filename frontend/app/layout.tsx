4cde2dbd3d890ec5309f4f2eda98cf7f59f120e1
Zion <ziontanyien@gmail.com>
2026-02-05 16:17:25 +0800

    CT4-59 Added Spinner logic and mock spinning in register page

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Loader from "@/app/components/Spinner";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,7 @@ export default function RootLayout({
       <body
         className="antialiased"
       >
-        {children}
+        <Loader>{children}</Loader>
       </body>
     </html>
   );
