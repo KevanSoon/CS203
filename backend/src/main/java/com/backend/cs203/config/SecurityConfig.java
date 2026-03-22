b0c9bc921c0831119629f8f22e8a43051ce8bf2c
ashmus <ashmus.codes@gmail.com>
2026-03-22 21:30:11 +0800

    enabled csrf, lesson thumbnail fixed

--- backend/src/main/java/com/backend/cs203/config/SecurityConfig.java
+++ backend/src/main/java/com/backend/cs203/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             // Disable CSRF (not needed for stateless JWT auth)
-            // .csrf(csrf -> csrf.disable())
+            .csrf(csrf -> csrf.disable())
             
             // Enable CORS, custom configuration
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
